faa2ca6e380c10b5b22289b83dfad16a11c2069e
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-01-06 14:40:57 -0800

    GPL added

--- scripts/Run_gpCAM.py
+++ scripts/Run_gpCAM.py
@@ -11,7 +11,41 @@ def main():
     from gpcam.main import main as main2
     from pathlib import Path
     #license here
-    input()
+    print("\n\
+    *** License Agreement ***\n\
+    \n\
+    GPL v3 License\n\
+    \n\
+    gpCAM Copyright (c) 2021, The Regents of the University of California,\n\
+    through Lawrence Berkeley National Laboratory (subject to receipt of\n\
+    any required approvals from the U.S. Dept. of Energy). All rights reserved.\n\
+    \n\
+    This program is free software: you can redistribute it and/or modify\n\
+    it under the terms of the GNU General Public License as published by\n\
+    the Free Software Foundation, either version 3 of the License, or\n\
+    (at your option) any later version.\n\
+    \n\
+    This program is distributed in the hope that it will be useful,\n\
+    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+    GNU General Public License for more details.\n\
+    \n\
+    You should have received a copy of the GNU General Public License\n\
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.\n\
+    \n\
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'\n\
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE\n\
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE\n\
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE\n\
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL\n\
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR \n\
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER\n\
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR\n\
+    TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF\n\
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\
+    ")
+    input("Press ENTER to agree to the terms of the license and to continue.")
+
 
     if len(sys.argv) == 1:
         main2()
