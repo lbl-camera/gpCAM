3558bb72956281cb3f7f12d6ddc4be4e8c8882a7
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-01-22 10:16:09 -0800

    Run_Visualization.py changed

--- scripts/Run_Visualization.py
+++ scripts/Run_Visualization.py
@@ -7,7 +7,7 @@ import gpcam
 import Config
 gpcam.global_config = Config
 from gpcam import global_config
-from gpcam.visualize import main
+from gpcam.visualize import main as mainv
 from pathlib import Path
 #license here
 
@@ -51,9 +51,9 @@ def main():
     if len(sys.argv) == 1:
         print("please specify data file via command line parameter, e.g. python Run_Visualization ../data/current_data/Data.npy");exit()
     if len(sys.argv) == 2:
-        main(data_path = sys.argv[1])
+        mainv(data_path = sys.argv[1])
     if len(sys.argv) == 3:
-        main(data_path = sys.argv[1], hyperparameter_path = sys.argv[2])
+        mainv(data_path = sys.argv[1], hyperparameter_path = sys.argv[2])
 if __name__ == "__main__":
     try:
         main()
