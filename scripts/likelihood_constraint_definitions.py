71a3641f7e172712e0df537e10827f9b331c9f78
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-02-01 11:13:14 -0800

    working on general constraints

--- scripts/likelihood_constraint_definitions.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-#####future development
-
-
---
+++ scripts/training_function.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+############################################################
+####this serves as an alternative to the log-likelihood#####
+####training################################################
+####one could imagine training a GP by minimizing a KL-Div##
+############################################################
+
+
+
+def my_training_function(hyperparameters):
+    ##something
+    something_that_will_be_minimized = 0.0
+    return something_that_will_be_minimized
+
+
+
+def my_training_function_grad(hyperparameters):
+    ##something
+    something_that_will_be_minimized = 0.0
+    return something_that_will_be_minimizedi
+
+def my_training_hess(hyperparameters):
+    ##something
+    something_that_will_be_minimized = 0.0
+    return something_that_will_be_minimized
