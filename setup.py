43f9042fa90f244496383acaded42b0845d99ac0
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-06-29 11:36:28 -0700

    homepage added

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     name='gpcam',
     packages=find_packages(include=['gpcam', 'gpcam.*']),
     test_suite='tests',
-    url='https://github.com/Marcus Noack/gpcam',
-    version='7.0.5',
+    url='https://gpcam.lbl.gov',
+    version='7.0.6',
     zip_safe=False,
 )
