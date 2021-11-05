8a97b198d68aa7da69bcc021732fd02aabe9c462
Marcus Michael Noack <MarcusNoack@lbl.gov>
2021-11-05 11:30:55 -0700

    numba removed

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     packages=find_packages(include=['gpcam', 'gpcam.*']),
     test_suite='tests',
     url='https://gpcam.lbl.gov',
-    version='7.3.0',
+    version='7.3.1',
     zip_safe=False,
 )
