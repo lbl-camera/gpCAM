cf2df0773e8880284837621c3a00e9f3367b6f2c
ronpandolfi <ronpandolfi@gmail.com>
2022-01-10 12:21:09 -0800

    Fix broken ref

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from os import path
 
 import versioneer
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
