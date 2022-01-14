c5dcd78f1747038b2619e804159d9abcfe758244
Ian Humphrey <ian.r.humphrey@gmail.com>
2022-01-14 14:09:59 -0800

    remove code duplication on update cost funcs

--- scripts/cost_function.py
+++ scripts/cost_function.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import numpy as np
 from scipy.optimize import differential_evolution as devo
 
@@ -24,31 +26,31 @@ def l1_cost(origin,x,arguments = None):
 ######update functions##################
 ########################################
 
-def update_l2_cost_function(costs, parameters):
+def _update_cost_function(costs,
+                          parameters,
+                          cost_func: Callable,
+                          compute_cost_misfit_func: Callable):
     print("Cost adjustment in progress...")
-    print("old cost parameters: ",parameters)
-    #print(costs)
+    print("old cost parameters: ", parameters)
     bounds = 0.0
-    #print(bounds)
-    #input()
     ###remove out-liers:
     origins = []
     points = []
     motions = []
-    c   = []
+    c = []
     cost_per_motion = []
     for i in range(len(costs)):
         origins.append(costs[i]["origin"])
         points.append(costs[i]["point"])
         motions.append(abs(costs[i]["origin"] - costs[i]["point"]))
         c.append(costs[i]["cost"])
-        cost_per_motion.append(costs[i]["cost"]/l2_cost(costs[i]["origin"],costs[i]["point"], parameters))
+        cost_per_motion.append(costs[i]["cost"] / cost_func(costs[i]["origin"], costs[i]["point"], parameters))
     mean_costs_per_distance = np.mean(np.asarray(cost_per_motion))
     sd = np.std(np.asarray(cost_per_motion))
     for element in cost_per_motion:
         if (
-            element >= mean_costs_per_distance - 2.0 * sd
-            and element <= mean_costs_per_distance + 2.0 * sd
+                element >= mean_costs_per_distance - 2.0 * sd
+                and element <= mean_costs_per_distance + 2.0 * sd
         ):
             continue
         else:
@@ -57,46 +59,25 @@ def update_l2_cost_function(costs, parameters):
             origins.pop(cost_per_motion.index(element))
             points.pop(cost_per_motion.index(element))
             cost_per_motion.pop(cost_per_motion.index(element))
-    res = devo(compute_l2_cost_misfit, bounds, args = (origins, points,c), tol=1e-6, disp=True, maxiter=300, popsize=20,polish=False)
-    arguments = {"offset": res["x"][0],"slope": res["x"][1]}
+    res = devo(compute_cost_misfit_func,
+               bounds,
+               args=(origins, points, c),
+               tol=1e-6,
+               disp=True,
+               maxiter=300,
+               popsize=20,
+               polish=False)
+    arguments = {"offset": res["x"][0], "slope": res["x"][1]}
     print("New cost parameters: ", arguments)
     return arguments
+
+
+def update_l2_cost_function(costs, parameters):
+    return _update_cost_function(costs, parameters, l2_cost, compute_l2_cost_misfit)
 
 
 def update_l1_cost_function(costs, parameters):
-    print("Cost adjustment in progress...")
-    print("old cost parameters: ",parameters)
-    bounds = 0.0
-    ###remove outliers:
-    origins = []
-    points = []
-    motions = []
-    c   = []
-    cost_per_motion = []
-    for i in range(len(costs)):
-        origins.append(costs[i]["origin"])
-        points.append(costs[i]["point"])
-        motions.append(abs(costs[i]["origin"] - costs[i]["point"]))
-        c.append(costs[i]["cost"])
-        cost_per_motion.append(costs[i]["cost"]/l1_cost(costs[i]["origin"],costs[i]["point"], parameters))
-    mean_costs_per_distance = np.mean(np.asarray(cost_per_motion))
-    sd = np.std(np.asarray(cost_per_motion))
-    for element in cost_per_motion:
-        if (
-            element >= mean_costs_per_distance - 2.0 * sd
-            and element <= mean_costs_per_distance + 2.0 * sd
-        ):
-            continue
-        else:
-            motions.pop(cost_per_motion.index(element))
-            c.pop(cost_per_motion.index(element))
-            origins.pop(cost_per_motion.index(element))
-            points.pop(cost_per_motion.index(element))
-            cost_per_motion.pop(cost_per_motion.index(element))
-    res = devo(compute_l1_cost_misfit, bounds, args = (origins, points,c), tol=1e-6, disp=True, maxiter=300, popsize=20,polish=False)
-    arguments = {"offset": res["x"][0],"slope": res["x"][1:]}
-    print("New cost parameters: ", arguments)
-    return arguments
+    return _update_cost_function(costs, parameters, l2_cost, compute_l2_cost_misfit)
 
 ########################################################
 ########################################################
