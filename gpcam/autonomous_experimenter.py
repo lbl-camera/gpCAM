d51c44584e9e042d3f04b7c56416e0e1b02ec496
elliott <elliott.perryman@hey.com>
2022-08-12 10:18:45 +0200

    train if passed a checkpoint

--- gpcam/autonomous_experimenter.py
+++ gpcam/autonomous_experimenter.py
@@ -382,8 +382,6 @@ class AutonomousExperimenterGP():
         i = 0
         n_measurements = len(self.x)
         while n_measurements < N:
-            i += 1
-            n_measurements = len(self.x)
             logger.info("----------------------------")
             logger.info(f"iteration {i}")
             logger.info(f"Run Time: {time.time() - start_time} seconds")
@@ -442,27 +440,28 @@ class AutonomousExperimenterGP():
                 ++++++++++++++++++++++++++
                 |Training ...            |
                 ++++++++++++++++++++++++++"""))
-            if n_measurements in retrain_async_at and n_measurements<N:
+            if any(n in retrain_async_at for n in range(n_measurements,len(self.x))) and n_measurements<N:
                 logger.info("    Starting a new asynchronous training after killing the current one.")
                 print("    Starting a new asynchronous training after killing the current one.")
                 self.kill_training()
                 self.train_async(max_iter=training_opt_max_iter,
                                  dask_client=self.training_dask_client)
-            elif n_measurements in retrain_globally_at and n_measurements<N:
+            elif any(n in retrain_globally_at for n in range(n_measurements,len(self.x))) and n_measurements<N:
                 self.kill_training()
                 logger.info("    Fresh optimization from scratch via global optimization")
                 self.train(pop_size=training_opt_pop_size,
                            tol=training_opt_tol,
                            max_iter=training_opt_max_iter,
                            method="global")
-            elif n_measurements in retrain_locally_at and n_measurements<N:
+            elif any(n in retrain_locally_at for n in range(n_measurements,len(self.x))) and n_measurements<N:
                 self.kill_training()
-                logger.info("    Fresh optimization from scratch via global optimization")
+                logger.info("    Fresh optimization from scratch via local optimization")
                 self.train(pop_size=training_opt_pop_size,
                            tol=training_opt_tol,
                            max_iter=training_opt_max_iter,
                            method="local")
             else:
+                print('no training at len',len(self.x))
                 logger.info("    No training in this round but I am trying to update the hyperparameters")
                 self.update_hps()
             logger.info(inspect.cleandoc("""#
@@ -483,6 +482,8 @@ class AutonomousExperimenterGP():
             if i in update_cost_func_at: self.gp_optimizer.update_cost_function(self.c)
 
             if error < breaking_error: break
+            i += 1
+            n_measurements = len(self.x)
         logger.info("killing the client... and then we are done")
         self.kill_client()
 
