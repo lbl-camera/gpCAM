f9efc6109de88cfde91ba1f13a30cd25efcc955a
Ronald Pandolfi <ronpandolfi@gmail.com>
2024-05-30 16:33:05 -0700

    transition to pyproject.toml and hatch; remove versioneer; add 3.11/3.12 to Actions CI,

--- gpcam/__init__.py
+++ gpcam/__init__.py
@@ -1,6 +1,9 @@
-from . import _version
+try:
+    from ._version import __version__
+except ImportError as ex:
+    raise RuntimeError('Running gpcam from source code requires installation. If you would like an editable source '
+                       'install, use "pip install -e ." to perform and editable installation.') from ex
 from loguru import logger
-import sys
 
 from .gp_optimizer import GPOptimizer
 from .gp_optimizer import fvGPOptimizer
@@ -8,7 +11,5 @@ from .autonomous_experimenter import AutonomousExperimenterGP
 from .autonomous_experimenter import AutonomousExperimenterFvGP
 
 __all__ = ['GPOptimizer', 'fvGPOptimizer','AutonomousExperimenterGP','AutonomousExperimenterFvGP']
-
-__version__ = _version.get_versions()['version']
 
 logger.disable('gpcam')
