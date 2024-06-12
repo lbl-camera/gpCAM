503b182a9afa95e1949764dc0ea9f8759c3f3678
Ronald Pandolfi <ronpandolfi@gmail.com>
2024-06-12 12:59:22 -0700

    expand error capture when importing _version.py

--- gpcam/__init__.py
+++ gpcam/__init__.py
@@ -1,6 +1,6 @@
 try:
     from ._version import __version__
-except ImportError as ex:
+except (ImportError, ModuleNotFoundError) as ex:
     raise RuntimeError('Running gpcam from source code requires installation. If you would like an editable source '
                        'install, use "pip install -e ." to perform and editable installation.') from ex
 from loguru import logger
